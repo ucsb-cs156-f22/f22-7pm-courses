fe89b7eeb3fee7ccb4ed34d1009160ea5b91d757
Caleb Lee <caleb_lee@ucsb.edu>
2022-11-16 19:36:55 -0800

    finished issue 4

--- src/main/java/edu/ucsb/cs156/courses/controllers/PersonalSchedulesController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/PersonalSchedulesController.java
@@ -90,10 +90,10 @@ public class PersonalSchedulesController extends ApiController {
         log.info("currentUser={}", currentUser);
 
         Iterable<PersonalSchedule> personalschedules = personalscheduleRepository.findAllByUserId(currentUser.getUser().getId());
-        for (PersonalSchedule p : personalschedules) {
-          if (p.getName() == name && p.getQuarter() == quarter) {
+        for (PersonalSchedule ps : personalschedules) {
+          if (ps.getName() == name && ps.getQuarter() == quarter) {
             //a schedule with the same name and quarter already exist, so throw exception
-            
+            throw new NameAndQuarterExistsException(ps.getName(), ps.getQuarter());
           }
         }
 
--- src/main/java/edu/ucsb/cs156/courses/errors/NameAndQuarterExistsException.java
+++ src/main/java/edu/ucsb/cs156/courses/errors/NameAndQuarterExistsException.java
@@ -2,7 +2,7 @@ package edu.ucsb.cs156.courses.errors;
 
 public class NameAndQuarterExistsException extends RuntimeException{
     public NameAndQuarterExistsException(String name, String quarter) {
-        super("Name and Quarter are invalid, (There already exists a course named name during quarter)"
+        super("Name and Quarter are invalid, (There already exists a course named %s during %s)"
         .formatted(name.toString(), quarter.toString()));
     }
 }
