0020267511279d8cbc212eb35622043ac9fc07f6
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2022-11-16 19:45:02 -0800

    dc - changed string formatting

--- src/main/java/edu/ucsb/cs156/courses/errors/NameAndQuarterExistsException.java
+++ src/main/java/edu/ucsb/cs156/courses/errors/NameAndQuarterExistsException.java
@@ -2,7 +2,7 @@ package edu.ucsb.cs156.courses.errors;
 
 public class NameAndQuarterExistsException extends RuntimeException{
     public NameAndQuarterExistsException(String name, String quarter) {
-        super("Name and Quarter are invalid, (There already exists a course named name during quarter)"
+        super("Name and Quarter are invalid, (There already exists a course named name and quarter)"
         .formatted(name.toString(), quarter.toString()));
     }
 }
