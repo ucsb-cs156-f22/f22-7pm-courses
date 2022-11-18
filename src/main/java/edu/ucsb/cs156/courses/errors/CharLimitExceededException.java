46d52cbbc9d5fa676080121523f272739d2f07de
Matthew Nguyen <matthewnguyen@ucsb.edu>
2022-11-17 17:36:16 -0800

    implemented name char limit for backend. included exception in apicontroller.java

--- src/main/java/edu/ucsb/cs156/courses/errors/CharLimitExceededException.java
+++ src/main/java/edu/ucsb/cs156/courses/errors/CharLimitExceededException.java
@@ -2,6 +2,6 @@ package edu.ucsb.cs156.courses.errors;
 
 public class CharLimitExceededException extends RuntimeException {
   public CharLimitExceededException(String name) {
-    super("Name: %s too long (Name must be no more than 15 characters)".formatted(name.toString()));
+    super("Name: %s too long (Name must be no more than 15 characters)".formatted(name));
   }
 }
