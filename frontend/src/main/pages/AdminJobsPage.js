dcbb8c1556849d9a06d7301115c547ac5e9881b1
Abelatnafu <Abeljayirv@gmail.com>
2022-11-28 10:27:31 -0800

    Try fixing Job problem

--- frontend/src/main/pages/AdminJobsPage.js
+++ frontend/src/main/pages/AdminJobsPage.js
@@ -36,7 +36,7 @@ const AdminJobsPage = () => {
     // ***** update courses job *******
 
     const objectToAxiosParamsUpdateCoursesJob = (data) => ({
-        url: `/api/jobs/launch/updateCourses?quarterYYYYQ=${data.quarter}&subjectArea=${data.subject}`,
+        url: `/api/jobs/launch/updateCourses?quarterYYYYQ=${data.quarter}&subjectArea=${data?.subject}`,
         method: "POST"
     });
 
