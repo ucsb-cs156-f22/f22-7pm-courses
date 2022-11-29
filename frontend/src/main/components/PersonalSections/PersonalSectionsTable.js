c14d2d5a47308ba27bce4a5da841c3c51c43c427
Matthew Nguyen <matthewnguyen@ucsb.edu>
2022-11-28 19:11:32 -0800

    mn - removed unused commented code

--- frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
+++ frontend/src/main/components/PersonalSections/PersonalSectionsTable.js
@@ -4,9 +4,6 @@ import SectionsTableBase from "main/components/SectionsTableBase";
 import { convertToFraction, formatDays, formatInstructors, formatLocation, formatTime } from "main/utils/sectionUtils.js";
 
 
-// function getFirstVal(values){
-//     return values[0];
-// }
 
 export default function PersonalSectionsTable({ personalSections }) {
 
@@ -23,72 +20,47 @@ export default function PersonalSectionsTable({ personalSections }) {
             Header: 'Enroll Code',
             accessor: 'classSections[0].enrollCode', 
             disableGroupBy: true,
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Section',
             accessor: "classSections[0].section",
             disableGroupBy: true,
-
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
         },
         {
             Header: 'Title',
             accessor: 'title',
-            disableGroupBy: true,
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
+            disableGroupBy: true
         },
 
         {
             Header: 'Enrolled',
             accessor: (row) => convertToFraction(row.classSections[0].enrolledTotal, row.classSections[0].maxEnroll),
             disableGroupBy: true,
-            id: 'enrolled',
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
+            id: 'enrolled'
         },
         {
             Header: 'Location',
             accessor: (row) => formatLocation(row.classSections[0].timeLocations),
             disableGroupBy: true,
-            id: 'location',
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
+            id: 'location'
         },
         {
             Header: 'Days',
             accessor: (row) => formatDays(row.classSections[0].timeLocations),
             disableGroupBy: true,
-            id: 'days',
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
+            id: 'days'
         },
         {
             Header: 'Time',
             accessor: (row) => formatTime(row.classSections[0].timeLocations),
             disableGroupBy: true,
-            id: 'time',
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
+            id: 'time'
         },
         {
             Header: 'Instructor',
             accessor: (row) => formatInstructors(row.classSections[0].instructors),
             disableGroupBy: true,
-            id: 'instructor',
-
-            // aggregate: getFirstVal,
-            // Aggregated: ({ cell: { value } }) => `${value}`
+            id: 'instructor'
         }   
     ];
 
