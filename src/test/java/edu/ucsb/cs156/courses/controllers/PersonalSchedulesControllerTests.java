46a76224003ee97b715debb4c4855e4b0ae99247
Caleb Lee <caleb_lee@ucsb.edu>
2022-11-29 11:28:09 -0800

    removed more uneccessary comments

--- src/test/java/edu/ucsb/cs156/courses/controllers/PersonalSchedulesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/PersonalSchedulesControllerTests.java
@@ -536,7 +536,6 @@ public void api_schedules__user_logged_in__can_put_schedule_that_does_not_alread
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened2").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-//     String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findByIdAndUser(eq(77L), eq(user))).thenReturn(Optional.of(ps1));
@@ -570,7 +569,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened1").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-//     String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findByIdAndUser(eq(77L), eq(user))).thenReturn(Optional.of(ps1));
@@ -674,7 +672,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps3 = PersonalSchedule.builder().name("Name 2").description("Description 1").quarter("20224").user(user).id(77L).build();
         PersonalSchedule ps4 = PersonalSchedule.builder().name("Name 3").description("Description 1").quarter("20221").user(user).id(77L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findById(eq(77L))).thenReturn(Optional.of(ps1));
@@ -710,7 +707,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened2").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findById(eq(77L))).thenReturn(Optional.of(ps1));
@@ -746,7 +742,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened1").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findById(eq(77L))).thenReturn(Optional.of(ps1));
