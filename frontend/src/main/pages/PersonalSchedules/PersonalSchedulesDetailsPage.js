89677782f312aa57c042feec28f65e9977499aa8
Matthew Nguyen <matthewnguyen@ucsb.edu>
2022-11-29 17:34:20 -0800

    Inserted PersonalSectionTable into PersonalSchedulesDetailsPage

--- frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailsPage.js
+++ frontend/src/main/pages/PersonalSchedules/PersonalSchedulesDetailsPage.js
@@ -2,12 +2,13 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
 import PersonalSchedulesTable from 'main/components/PersonalSchedules/PersonalSchedulesTable';
 import { useBackend, _useBackendMutation } from "main/utils/useBackend";
+import PersonalSectionsTable from "main/components/PersonalSections/PersonalSectionsTable";
 
 
 export default function PersonalSchedulesDetailsPage() {
   let { id } = useParams();
 
-  const { data: personalSchedule, _error, _status } =
+  const { data: personalSchedule, _scheduleError, _scheduleStatus } =
     useBackend(
         // Stryker disable all : hard to test for query caching
       [`/api/personalschedules?id=${id}`],
@@ -20,6 +21,19 @@ export default function PersonalSchedulesDetailsPage() {
       }
     );
 
+  const { data: personalSections, _error, _status } =
+  useBackend(
+      // Stryker disable all : hard to test for query caching
+    [`/api/personalSections/all`],
+    {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+      method: "GET",
+      url: `/api/personalSections/all?psId=${id}`,
+      params: {
+        id
+      }
+    }
+  );
+
   return (
     <BasicLayout>
       <div className="pt-2">
@@ -27,9 +41,8 @@ export default function PersonalSchedulesDetailsPage() {
         {personalSchedule &&
                 <PersonalSchedulesTable personalSchedules={[personalSchedule]} showButtons={false} />
             }
-            <p>
-                This is where the list of courses will go (from PersonalSectionsTable)
-            </p>
+        {personalSections && <PersonalSectionsTable personalSections={personalSections} />
+            }
       </div>
     </BasicLayout>
   )
