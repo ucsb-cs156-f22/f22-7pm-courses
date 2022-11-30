f04b875380c7dc4d5cd9d15f811d03c8d57dcee8
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2022-11-29 17:09:09 -0800

    dc - got rid of commented out ps1string

--- src/test/java/edu/ucsb/cs156/courses/controllers/PersonalSchedulesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/PersonalSchedulesControllerTests.java
@@ -501,7 +501,6 @@ public class PersonalSchedulesControllerTests extends ControllerTestCase {
         PersonalSchedule ps1 = PersonalSchedule.builder().name("Name 1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("Name 2").description("Description 2").quarter("20222").user(user).id(78L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findByIdAndUser(eq(77L), eq(user))).thenReturn(Optional.of(ps1));
@@ -536,7 +535,6 @@ public void api_schedules__user_logged_in__can_put_schedule_that_does_not_alread
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened2").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-//     String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findByIdAndUser(eq(77L), eq(user))).thenReturn(Optional.of(ps1));
@@ -570,7 +568,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened1").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-//     String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findByIdAndUser(eq(77L), eq(user))).thenReturn(Optional.of(ps1));
@@ -672,7 +669,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1 = PersonalSchedule.builder().name("Name 1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("Name 2").description("Description 2").quarter("20222").user(user).id(78L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findById(eq(77L))).thenReturn(Optional.of(ps1));
@@ -708,7 +704,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened2").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findById(eq(77L))).thenReturn(Optional.of(ps1));
@@ -744,7 +739,6 @@ public void api_schedules__user_logged_in__can_put_schedule_with_quarter_that_do
         PersonalSchedule ps1Array = PersonalSchedule.builder().name("NameLengthened1").description("Description 1").quarter("20221").user(user).id(77L).build();
         PersonalSchedule ps2 = PersonalSchedule.builder().name("NameLengthened1").description("Description 2").quarter("20222").user(user).id(77L).build();
 
-        // String ps1String = mapper.writeValueAsString(ps1);
         String ps2String = mapper.writeValueAsString(ps2);
 
         when(personalscheduleRepository.findById(eq(77L))).thenReturn(Optional.of(ps1));
