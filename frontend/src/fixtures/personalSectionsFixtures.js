366ed219f45a250bc69739f10ccecc43ae1e4f0b
Matthew Nguyen <matthewnguyen@ucsb.edu>
2022-12-01 00:09:45 -0800

    implemented scraper function for courseIds with or without suffixes

--- frontend/src/fixtures/personalSectionsFixtures.js
+++ frontend/src/fixtures/personalSectionsFixtures.js
@@ -537,3 +537,127 @@ export const fiveSections = [
     "title": "PROBLEM SOLVING I"
   }
 ]
+
+export const sectionSuffix = [
+  {
+    "classSections": [
+      {
+        "classClosed": "string",
+        "concurrentCourses": [
+          "string"
+        ],
+        "courseCancelled": "string",
+        "departmentApprovalRequired": true,
+        "enrollCode": "12345",
+        "enrolledTotal": 83,
+        "gradingOptionCode": "string",
+        "instructorApprovalRequired": true,
+        "instructors": [
+          {
+            "functionCode": "string",
+            "instructor": "WANG L C"
+          }
+        ],
+        "maxEnroll": 100,
+        "restrictionLevel": "string",
+        "restrictionMajor": "string",
+        "restrictionMajorPass": "string",
+        "restrictionMinor": "string",
+        "restrictionMinorPass": "string",
+        "secondaryStatus": "string",
+        "section": "0100",
+        "session": "string",
+        "timeLocations": [
+          {
+            "beginTime": "15:30",
+            "building": "HFH",
+            "days": "M W    ",
+            "endTime": "16:45",
+            "room": "1104",
+            "roomCapacity": "string"
+          }
+        ]
+      }
+    ],
+    "courseId": "ECE       1A -1",
+    "description": "string",
+    "finalExam": {
+      "beginTime": "string",
+      "comments": "string",
+      "endTime": "string",
+      "examDate": "string",
+      "examDay": "string",
+      "hasFinals": true
+    },
+    "generalEducation": [
+      {
+        "geCode": "string",
+        "geCollege": "string"
+      }
+    ],
+    "quarter": "string",
+    "title": "COMP ENGR SEMINAR"
+  }
+]
+
+export const sectionNoSuffix = [
+  {
+    "classSections": [
+      {
+        "classClosed": "string",
+        "concurrentCourses": [
+          "string"
+        ],
+        "courseCancelled": "string",
+        "departmentApprovalRequired": true,
+        "enrollCode": "12345",
+        "enrolledTotal": 83,
+        "gradingOptionCode": "string",
+        "instructorApprovalRequired": true,
+        "instructors": [
+          {
+            "functionCode": "string",
+            "instructor": "WANG L C"
+          }
+        ],
+        "maxEnroll": 100,
+        "restrictionLevel": "string",
+        "restrictionMajor": "string",
+        "restrictionMajorPass": "string",
+        "restrictionMinor": "string",
+        "restrictionMinorPass": "string",
+        "secondaryStatus": "string",
+        "section": "0100",
+        "session": "string",
+        "timeLocations": [
+          {
+            "beginTime": "15:30",
+            "building": "HFH",
+            "days": "M W    ",
+            "endTime": "16:45",
+            "room": "1104",
+            "roomCapacity": "string"
+          }
+        ]
+      }
+    ],
+    "courseId": "ECE       1A ",
+    "description": "string",
+    "finalExam": {
+      "beginTime": "string",
+      "comments": "string",
+      "endTime": "string",
+      "examDate": "string",
+      "examDay": "string",
+      "hasFinals": true
+    },
+    "generalEducation": [
+      {
+        "geCode": "string",
+        "geCollege": "string"
+      }
+    ],
+    "quarter": "string",
+    "title": "COMP ENGR SEMINAR"
+  }
+]
